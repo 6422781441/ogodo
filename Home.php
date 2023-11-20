eea29c7ca8c5e7359305a49ddf5d7c2c86ddd49c
6422781441 <144684401+6422781441@users.noreply.github.com>
2023-11-20 15:53:45 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,3 +1,71 @@
-<?php>
+<?php
+    require_once("connect.php");
+   
+    $Ac_name = $_POST["Ac_name"];
+    $Address = $_POST["Address"];
+    $Ac_type = $_POST["Ac_type"];
 
-<?>
+        echo "You are searching for $Address" ;
+
+
+
+    if (is_null($Ac_name) && is_null($Address) && $Ac_type == "*") 
+        {  
+        $q = "Select * from accommodatiom ";
+        }
+
+    if ( !is_null($Ac_name) && is_null($Address) && $Ac_type == "*"){
+        $q = "Select * from accommodatiom where Ac_name LIKE '%$Ac_name%'";
+    }
+
+    if ( is_null($Ac_name) && !is_null($Address) && $Ac_type == "*"){
+        $q = "Select * from accommodatiom where Address LIKE '%$Address%'";
+    }
+    if ( !is_null($Ac_type) && !is_null($Address) && $Ac_type == "*"){
+        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_name LIKE '%$Ac_name%'";
+    }
+    
+
+
+    if (is_null($Ac_name) && is_null($Address) && $Ac_type != "*") {  
+        $q = "Select * from accommodatiom where Ac_type = '$Ac_type'";
+    }
+
+    if ( !is_null($Ac_name) && is_null($Address) && $Ac_type != "*"){
+        $q = "Select * from accommodatiom where Ac_name LIKE '%$Ac_name%' and Ac_type = '$Ac_type'";
+    }
+
+    if ( is_null($Ac_name) && !is_null($Address) && $Ac_type != "*"){
+        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_type = '$Ac_type'";
+    }
+    if ( !is_null($Ac_type) && !is_null($Address) && $Ac_type != "*"){
+        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_name LIKE '%$Ac_name%' and Ac_type ='$Ac_type'";
+    }
+    
+if($result= $mysqli->query($q)){
+echo'<table border="1">';
+echo'<tr><th>ID</th>
+<th>Name</th>
+<th>Type</th>
+<th>Address</th>
+<th>Rating</th>
+<th>Booking</th>
+</tr>';
+
+while($row= $result->fetch_array()){
+    echo"<tr>";
+    echo "<td>".$row["Ac_ID"]."</td>";
+    echo "<td>".$row["Ac_name"]."</td>";
+    echo "<td>".$row["Ac_type"]."</td>";
+    echo "<td>".$row["Address"]."</td>";
+    echo "<td>".$row["Rating"]."</td>";
+
+    echo "<td><a href='booking_Form.php?id=".$row['p_id']."'>Booking</a></td>";
+    echo"</tr>";
+}
+echo '</table>';
+$result->free();
+}else{
+    echo"Retrieval failed: ".$mysqli->error;
+}
+?>
