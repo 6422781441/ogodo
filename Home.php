4d077eb8d0a5cce7d5e153b9f2f904444ac6bb9d
6422781441 <144684401+6422781441@users.noreply.github.com>
2023-11-20 16:38:14 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,71 +1,31 @@
-<?php
-    require_once("connect.php");
-   
-    $Ac_name = $_POST["Ac_name"];
-    $Address = $_POST["Address"];
-    $Ac_type = $_POST["Ac_type"];
+<!DOCTYPE html>
 
-        
-    //echo "You are searching for every accommodation in our website" ;
+<html>
+    <head>
+        <title>Search for your accommodation</title>
+    </head>
+    <body> <h1>Search for your accommodation</h1>
 
+    <form action="search_Process.php" method="post">
 
-    if (is_null($Ac_name) && is_null($Address) && $Ac_type == "All") 
-        {      
-        $q = "Select * from accommodatiom ";
-        }
+                <label for="Ac_name">Accommodation Name:</label>
+                <input type="text" name="Ac_name" placeholder = "Give us a name"><br>
+                
 
-    if ( !is_null($Ac_name) && is_null($Address) && $Ac_type == "All"
-        $q = "Select * from accommodatiom where Ac_name = '$Ac_name '";
-    }
+                <label for="Address">Accommodation Address:</label>
+                <input type="text" name="Address" placeholder = "What is the address?"><br>
+                
 
-    if ( is_null($Ac_name) && !is_null($Address) && $Ac_type == "All")
-        $q = "Select * from accommodatiom where Address = '$Address'";
-    }
-    if ( !is_null($Ac_type) && !is_null($Address) && $Ac_type == "All" ){
-        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_name LIKE '%$Ac_name%'";
-    }
-    
-
-
-    if (is_null($Ac_name) && is_null($Address) && $Ac_type != "All" ) {  
-        $q = "Select * from accommodatiom where Ac_type = '$Ac_type'";
-    }
-
-    if ( !is_null($Ac_name) && is_null($Address) && $Ac_type != "All" ){
-        $q = "Select * from accommodatiom where Ac_name LIKE '%$Ac_name%' and Ac_type = '$Ac_type'";
-    }
-
-    if ( is_null($Ac_name) && !is_null($Address) && $Ac_type != "All" ){
-        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_type = '$Ac_type'";
-    }
-    if ( !is_null($Ac_type) && !is_null($Address) && $Ac_type != "All"  ){
-        $q = "Select * from accommodatiom where Address LIKE '%$Address%' and Ac_name LIKE '%$Ac_name%' and Ac_type ='$Ac_type'";
-    }
-    
-if($result= $mysqli->query($q)){
-echo'<table border="1">';
-echo'<tr><th>ID</th>
-<th>Name</th>
-<th>Type</th>
-<th>Address</th>
-<th>Rating</th>
-<th>Booking</th>
-</tr>';
-
-while($row= $result->fetch_array()){
-    echo"<tr>";
-    echo "<td>".$row["Ac_ID"]."</td>";
-    echo "<td>".$row["Ac_name"]."</td>";
-    echo "<td>".$row["Ac_type"]."</td>";
-    echo "<td>".$row["Address"]."</td>";
-    echo "<td>".$row["Rating"]."</td>";
-
-    echo "<td><a href='booking_Form.php?id=".$row['p_id']."'>Booking</a></td>";
-    echo"</tr>";
-}
-echo '</table>';
-$result->free();
-}else{
-    echo"Retrieval failed: ".$mysqli->error;
-}
-?>
+                <label for="Ac_type">Accommodation Type:</label>
+                <select name="Ac_type" id="type">
+                <option value= "All" >All</option>
+                <option value="Pool Villa">Pool Villa</option>
+                <option value="Hotel">Hotel</option>
+                <option value="Apartment">Apartment</option>
+                <option value="House">House</option>
+                </select><br>
+                
+                <button type = "submit">Search</button><br>
+                
+    </body>
+</html>
